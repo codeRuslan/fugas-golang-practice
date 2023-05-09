3e2e6cde8adc817e4428ed070fda764615e6d0bf
Ruslan Pylypiuk <ruslan.pilipyuk@gmail.com>
2023-05-09 12:43:31 +0100

    Adding Swager

--- 5-task/main.go
+++ 5-task/main.go
@@ -12,7 +12,6 @@ func main() {
 		entity.Book{Name: "Philosopher's Stone", Author: "J. K. Rowling", Year: 1997},
 		entity.Book{Name: "All Quiet on the Western Front", Author: "Erich Maria Remarque", Year: 1929},
 	}
-
 	bookstore := store.NewBook(books)
 	handlers.HandleRequests(bookstore)
 }
