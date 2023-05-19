37cee083685feccc12d565809cb20a796b87ae2f
Ruslan Pylypiuk <ruslan.pilipyuk@gmail.com>
2023-05-19 01:06:58 +0100

    Integrated CSV Repo fully into API, removed comments, added documentation

--- 6-task-csv-repository/main.go
+++ 6-task-csv-repository/main.go
@@ -3,18 +3,15 @@ package main
 import (
 	"awesomeProject1/config"
 	"awesomeProject1/handlers"
-	"awesomeProject1/repository"
 	"awesomeProject1/store"
 )
 
 func main() {
-	/*books := []entity.Book{
-		entity.Book{Name: "Rage", Author: "Stephen King", Year: 1977}, Rage,Stephen King,1977
-		entity.Book{Name: "Philosopher's Stone", Author: "J. K. Rowling", Year: 1997}, Philosopher's Stone,J. K. Rowling,1997
-		entity.Book{Name: "All Quiet on the Western Front", Author: "Erich Maria Remarque", Year: 1929}, All Quiet on the Western Front,Erich Maria Remarque,1929
-	}*/
-	configFile, _ := config.ReadJsonConfigFile("/Users/ruslanpilipyuk/GolandProjects/awesomeProject1/config/config.json")
-	books := repository.GetCSVBooks(configFile.FilePath)
+	configFile, err := config.ReadJsonConfigFile("/Users/ruslanpilipyuk/GolandProjects/awesomeProject1/config/config.json")
+	if err != nil {
+		panic(err)
+	}
+	books := store.ReadBooksInCSV(configFile.FilePath)
 	bookstore := store.NewBook(books)
 	handlers.HandleRequests(bookstore)
 }
--- 6-task-csv-repository/store/bookstore.go
+++ 6-task-csv-repository/store/bookstore.go
@@ -3,7 +3,8 @@ package store
 import (
 	"awesomeProject1/config"
 	"awesomeProject1/entity"
-	"awesomeProject1/repository"
+	"github.com/gocarina/gocsv"
+	"os"
 )
 
 type Book interface {
@@ -26,8 +27,44 @@ func (bl *book) GetAll() []entity.Book {
 func (bl *book) Update(books []entity.Book) ([]entity.Book, error) {
 	bl.Books = books
 	configFile, _ := config.ReadJsonConfigFile("/Users/ruslanpilipyuk/GolandProjects/awesomeProject1/config/config.json")
-	repository.UpdateCSVBooks(configFile.FilePath, bl.Books)
+	//UpdateCSVBooks(configFile.FilePath, bl.Books)
+
+	file, err := os.OpenFile(configFile.FilePath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		return nil, err
+	}
+
+	defer file.Close()
+	if err := gocsv.MarshalWithoutHeaders(&books, file); err != nil {
+		panic(err)
+	}
+
 	return bl.Books, nil
-	//bl.Books = books
-	//return bl.Books, nil
 }
+
+func ReadBooksInCSV(path string) []entity.Book {
+	bookCSVfile, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE, os.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+	defer bookCSVfile.Close()
+
+	BooksCSV := []entity.Book{}
+
+	if err := gocsv.UnmarshalFile(bookCSVfile, &BooksCSV); err != nil {
+		panic(err)
+	}
+
+	return BooksCSV
+}
+
+/*func UpdateCSVBooks(path string, books []entity.Book) ([]entity.Book, error) {
+	file, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
+
+	defer file.Close()
+	if err := gocsv.MarshalWithoutHeaders(&books, file); err != nil {
+		panic(err)
+	}
+
+	return books, err
+}*/
